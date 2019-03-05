963705692aca80e014961a610adca9befeeba803
Grovesy101 <99tgroves@gmail.com>
2019-03-05 18:57:29 +0000

    Just some enetities for the prison

---
+++ src/com/KPMS/entites/Cell.java
@@ -0,0 +1,26 @@
+package com.KPMS.entites;
+
+public class Cell {
+	
+	private boolean occupied;
+	private String securityLevel;
+	private String cellNumber;
+	private String block;
+	
+	private int capacityTotal;
+	private int capactiyUsed;
+	
+	private int prisonerID;
+	
+	private int lastCheck;
+	private int nextCheck;
+	
+	public Cell() {
+		
+	}
+	
+	private void RequestCheck() {
+		
+	}
+
+}
---
+++ src/com/KPMS/entites/Prisoner.java
@@ -0,0 +1,29 @@
+package com.KPMS.entites;
+
+public class Prisoner {
+
+	private String prisonerName;
+	private int prisonerID;
+	private int sentenceLength;
+	
+	//release date
+	private int releaseDay, releaseMonth, releaseYear;
+	
+	private String[] medication;
+	
+	private String job;
+	
+	private String securityLevel;
+	
+	public Prisoner(String prisonerFilePath) {
+		
+	}
+	
+	public void DeletePrisoner() {
+		
+	}
+	
+	public void GetBehaviorRecords() {
+		
+	}
+}
